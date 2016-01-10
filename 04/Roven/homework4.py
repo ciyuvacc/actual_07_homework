db61692494a8932ef87cc09062cf9b7b346ee48c
MissRoven <missrovent@gmail.com>
2016-01-10 09:46:30 +0800

    Revert "Revert "change for hoemwork4""
    This reverts commit 0364d05b04d31499be461866b2b059911d933748.

--- 04/Roven/homework4.py
+++ 04/Roven/homework4.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 path1 = '/opt/python/www_access_20140823.log'
+#path1 = '/opt/python/www_access.log'
+path2 = '/opt/python/count.log'
 hd1 = open(path1,'r')
 tmp_list= []
 tmp_str= ''
@@ -11,9 +13,39 @@ for _line in hd1:
     tmp_dict[tmp_str] += 1
 hd1.close()
 #print tmp_dict
+
+'''
 show_list = []  
 for _key, _value in tmp_dict.items():
     show_list=_key.split()
     show_list[2]= (show_list[2], _value)
     print show_list
+'''
+
+
+
+tmp_dir = {}
+for _key, _value in tmp_dict.items():
+    tmp_dir.setdefault(_value, [])
+    tmp_dir[_value].append(_key)
+#print tmp_dir
+
+arr_list = tmp_dir.items()
+#print arr_list
+for i in range(0,len(arr_list)-1):
+    for j in range(0,len(arr_list)-i-1):
+        if arr_list[j][0] < arr_list[j+1][0]:
+            arr_list[j], arr_list[j+1] = arr_list[j+1], arr_list[j]
+#print arr_list
+
+tmp_list = []
+hd2 = open(path2,'w')
+for _list_line in arr_list:
+    for _tmp_line in _list_line[1]:
+         tmp_list =_tmp_line.split()
+         tmp_list[2] = (tmp_list[2], _list_line[0]) 
+         hd2.write(str(tmp_list)+ '\n')
+hd2.close()
+
+
 
