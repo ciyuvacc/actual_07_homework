07431516060bede81dbd2a7078bd43c965a6d410
XuHeng <xh0628@foxmail.com>
2016-01-11 14:19:06 +0800

    xuheng@1225

--- 02/xuheng/zuoye.py
+++ 02/xuheng/zuoye.py
@@ -1,21 +1,4 @@
 #coding=utf-8
-seq = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
-
-for i in range(1,len(seq)):
-        tmp=seq[i]
-        pos=i;
-        for j in range(i-1,-1,-1):
-            if seq[j]>tmp:
-                seq[j+1]=seq[j]
-                pos=j
-        seq[pos]=tmp
-       # print '排序分解:%s' %seq
-print '排序结果:%s' %seq #排序结果
-
-
-
-
-
 '''
 流程:
 1. 从seq索引为1的元素从左到右遍历
@@ -32,3 +15,38 @@ print '排序结果:%s' %seq #排序结果
 
 加油
 '''
+
+seq = [91,4,2,1,5,11,7,10,22,33,55]
+
+for i in range(1,len(seq)):
+        tmp=seq[i]
+        pos=i
+        for j in range(i-1,-1,-1):
+            if seq[j]>tmp:
+                seq[j+1]=seq[j]
+                pos=j
+        seq[pos]=tmp
+print '排序结果:%s' %seq #排序结果
+
+
+
+num_list = [91,4,2,1,5,11,7,10,22,33,55]
+
+start = 9
+end = -1
+step = -1
+
+for idx in range(start,end,step):
+    if num_list[idx] < num_list[idx-1]:
+        num_list[idx],num_list[idx-1] = num_list[idx-1],num_list[idx]
+    else:
+        break
+print num_list
+
+
+
+
+
+
+
+
