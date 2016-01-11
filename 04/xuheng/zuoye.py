02fb4e83bc539145721324437e936151b44fa5f5
XuHeng <xh0628@foxmail.com>
2016-01-11 14:25:48 +0800

    xuheng@1225

--- 04/xuheng/zuoye.py
+++ 04/xuheng/zuoye.py
@@ -1,35 +1,41 @@
-
-#coding=utf-8
-#试写 第一版本
-#coding=utf-8
-
-def read_file():
-            log_dict = {}
-            res_dict = []
-            f=open("www_access_20140823.log", "r")
-            contents = f.readlines()
-            f.close()
-            for line in contents:
-                log_ip=line.split()[0]
-                log_url=line.split()[6]
-                log_code=line.split()[8]
-                log_dict[(log_ip,log_url,log_code)]=log_dict.setdefault((log_ip,log_url,log_code), 0) +1
-
-            for k,v in log_dict.iteritems():
-                res_dict.append([k[2], k[1], (k[0], v)])
-
-            for l in sorted(res_dict, key=lambda x:x[2][1], reverse=True)[:10]:
-                 print l
-read_file()
+#!/bin/env python
+# -*- encoding:utf-8 -*-
 
 
+log_stat_dict = {}
+# 读取文件
+handle = open('www_access_20140823.log')
 
+while True:
+    _line = handle.readline()
+    if '' == _line:
+        break
+    _log_nodes = _line.split()
+    _ip = _log_nodes[0]
+    _url = _log_nodes[6]
+    _code = _log_nodes[8]
+    
+    _key = (_ip,_url,_code)
+    log_stat_dict.setdefault(_key,0)
+    log_stat_dict[_key] += 1
 
+handle.close()
 
-'''
-不错已经预习了函数的知识，结果正确
-可以改进的地方：
-1. 13,14,15使用3次split，可否改成一个？
-2. 注意缩进
+#print log_stat_dict
 
-'''
+log_stat_list = []
+for _key in log_stat_dict:
+    _value = log_stat_dict[_key]
+    _ip,_url,_code = _key
+    log_stat_list.append([_code,_url,(_ip,_value)])
+
+#print log_stat_list
+
+# top10
+for j in range(10):
+    for i in range(len(log_stat_list) - 1):
+        if log_stat_list[i][2][1] > log_stat_list[i + 1][2][1]:
+            log_stat_list[i],log_stat_list[i+1] = log_stat_list[i+1],log_stat_list[i]
+
+for node in log_stat_list[:-11:-1]:
+    print node
