b09572dc9505fe6bf23228c419a6bc5372298e6f
root <root@localhost.localdomain>
2016-01-14 18:02:40 +0800

    day5 homework

--- 05/songjincheng/work1.py
+++ 05/songjincheng/work1.py
@@ -11,6 +11,6 @@
 
 list_data = [(1,4),(5,1),(2,3)]
 #级别1：在lambda中用max
-print(sorted(list_data,key = lambda x: max(x)))
+print sorted(list_data,key = lambda x: max(x))
 #级别2：lambda中不用max
-print(sorted(list_data, key = lambda x:x if x[0] > x[1] else x[1]))
+print sorted(list_data, key = lambda x:x if x[0] > x[1] else x[1])
