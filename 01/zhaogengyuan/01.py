7c545ca28322cd8967df0bb2896ee7b806a47bc0
chaokenyuan@gmail.com <chaokenyuan@gmail.com>
2016-01-19 16:33:07 +0800

    add

--- 01/zhaogengyuan/01.py
+++ 01/zhaogengyuan/01.py
@@ -10,3 +10,20 @@ for i in List:
         max1 = i
 print max1
 print max2
+
+
+max1 = 0
+max2 = 0
+List =[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+
+for i in List:
+    if max1 >= max2 and max2 >= i:
+        continue
+    elif max1 >=i and i >= max2:
+        max2 = i
+    else:
+        max2 = max1
+        max1 = i
+print max1
+print max2
+
