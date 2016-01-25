ff6c3e628306a4ca8c025c1881dfc0a4278d5451
chaokenyuan@gmail.com <chaokenyuan@gmail.com>
2016-01-25 22:22:10 +0800

    fix

--- 07/zhaogengyuan/models.py
+++ 07/zhaogengyuan/models.py
@@ -15,3 +15,20 @@ def validate_user_login(username,password):
     cur.close()
     conn.close()
     return count != 0
+def user_register(username,password):
+    conn = MySQLdb.connect(host=gconf.DB_HOST, \
+            port=gconf.DB_PORT, \
+            user=gconf.DB_USER, \
+            passwd=gconf.DB_PASSWD, \
+            db=gconf.DB_DATABASE, \
+            charset=gconf.DB_CHARSET)
+    cur = conn.cursor()
+    count = cur.execute('SELECT * FROM user where username=%s and password=md5(%s)', \
+            (username,password))
+    cur.close()
+    conn.close()
+
+
+
+
+    return count != 0
--- 07/zhaogengyuan/views.py
+++ 07/zhaogengyuan/views.py
@@ -1,11 +1,11 @@
 #encoding: utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding=('utf-8')
 from flask import Flask
 from flask import render_template   
 from flask import request
 from flask import redirect
-import sys
-reload(sys)
-sys.setdefaultencoding=('utf-8')
 import models
 
 app=Flask(__name__)
@@ -21,29 +21,24 @@ def login():
     if models.validate_user_login(username,password):
         return redirect('/users/')
     else:
-		return render_template('index.html', \
-						Loginerror='用户名或密码错误', \
-						Loginusername=username, \
-						Loginpassword=password)
+	return render_template('index.html', \
+			loginerror='用户名或密码错误', \
+			loginusername=username, \
+			loginpassword=password)
 						
 @app.route('/register/')
-    username = request.form.get('username')
-	password = request.form.get('password','')
-	age = request.form.get('age','')
-	address = request.form.get('address','')
-	if models.validate_user_register(username,password,age,address):
-	    return redirect('/login/')
-	else
-	    return render_template('index.html', \
-		                Loginerror='用户名称重复', \
-						Loginusername=username, \
-						Loginpassword=password)
+def register():
+    username = request.form.get('username','')
+    password = request.form.get('password','')
+    age = request.form.get('age','')
+    address = request.form.get('address','')
+    if models.validate_user_register(username,password,age,address):
+        return redirect('/register/')
+    else:
+        return render_template('index.html')
 		
 def users():
     return '登录成功'
-	
-def register():
-    return '注册成功'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8888,debug=True)
