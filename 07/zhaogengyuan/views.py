c19991b448a5e715dd5fb1c453534f0a9bab59f8
Peter <chaokenyuan@gmail.com>
2016-01-25 16:06:37 +0800

    改

--- 07/zhaogengyuan/views.py
+++ 07/zhaogengyuan/views.py
@@ -9,6 +9,7 @@ sys.setdefaultencoding=('utf-8')
 import models
 
 app=Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,10 +25,25 @@ def login():
 						Loginerror='用户名或密码错误', \
 						Loginusername=username, \
 						Loginpassword=password)
+						
 @app.route('/register/')
     username = request.form.get('username')
+	password = request.form.get('password','')
+	age = request.form.get('age','')
+	address = request.form.get('address','')
+	if models.validate_user_register(username,password,age,address):
+	    return redirect('/login/')
+	else
+	    return render_template('index.html', \
+		                Loginerror='用户名称重复', \
+						Loginusername=username, \
+						Loginpassword=password)
+		
 def users():
     return '登录成功'
+	
+def register():
+    return '注册成功'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8888,debug=True)
