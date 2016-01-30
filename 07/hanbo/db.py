b6b8be3cf62fcaef6ea851d45237b6f6795b63ec
2012think@gmail.com <2012think@gmail.com>
2016-01-30 21:56:44 +0800

    day 7 zuoye （有点小问题）

---
+++ 07/hanbo/db.py
@@ -0,0 +1,14 @@
+#-----------------------------------------
+# File Name: db.py
+# Author: daboluo
+# mail: 429695690@qq.com
+# Created Time: Sunday, January 24, 2016 PM06:05:29 HKT
+#-----------------------------------------
+#!/usr/bin/env python
+#coding:utf8
+DB_HOST = 'localhost'
+DB_PORT = 3306
+DB_USER = 'root'
+DB_PASS = 'redhat'
+DB_DATABASE = 'hanbo'
+DB_CHARSET = 'utf8'
