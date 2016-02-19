403520aed5b7ac8f0ca64cc58fb5cc00a81db2c4
chaokenyuan@gmail.com <chaokenyuan@gmail.com>
2016-02-20 07:38:50 +0800

    views

--- 07/zhaogengyuan/views.py
+++ 07/zhaogengyuan/views.py
@@ -19,13 +19,45 @@ def login():
     username = request.form.get('username','')
     password = request.form.get('password','')
     if models.validate_user_login(username,password):
-        return redirect('/users/')
+        return redirect('/listUser/')
     else:
-	return render_template('index.html', \
-			loginerror='用户名或密码错误', \
-			loginusername=username, \
-			loginpassword=password)
-		
+        return render_template('login.html', \
+            login_error='用户名或密码错误')
+
+@app.route('/createUser/')
+def createUser():
+    return render_template('create.html')
+
+
+
+
+@app.route('/addUser/', methods=["POST"])
+def addUser():
+    username = request.form.get('username','')
+    password = request.form.get('password','')
+    age = request.form.get('age','')
+    address = request.form.get('address','')
+    if models.query_user(username):
+        return render_template('index.html', \
+                        register_error='注册失败，用户名重复', \
+                        username=username)
+    elif isNone(password):
+        return render_template('index.html', \
+                        register_error='注册失败，密码为空', \
+                        password=password)
+    elif isNotNum(age):
+        return render_template('index.html', \
+                        register_error='注册失败，年龄必须为数字', \
+                        username=username, \
+                        password=password, \
+                        age=age, \
+                        address=address)
+    else:
+        models.validate_user_add(username,password,age,address)
+        return '新增成功'
+
+
+ 
 @app.route('/register/', methods=["POST"])
 def register():
     username = request.form.get('username','')
@@ -51,13 +83,17 @@ def register():
         models.validate_user_add(username,password,age,address)
         return '注册成功'
 
-@app.route('/users/')		
-def users():
-    return '登录成功'
-
-@app.route('/userlist/')
-def userlist():
-    return render_template('list.html')
+#@app.route('/listUser/')
+@app.route('/listUser/',methods=['GET','POST'])
+def listUser():
+    username = request.form.get('query','')
+    users=models.query_user(username)
+    if users:
+        return render_template('list.html', \
+                    user=username, \
+                    users=users)
+    else:
+        return 'none'
 
 def isNone(string):
     if string:
@@ -80,3 +116,4 @@ def isNotNum(num):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8888,debug=True)
+
